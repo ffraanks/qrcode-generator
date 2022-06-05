c2bbe441a0d8344ac3588cd082e8dfe948c58d33
Viniciuscgobbi <vinicius.cgobbi2004@gmail.com>
2022-06-05 20:17:00 -0300

    Adicionado comando cls para limpeza do terminal no Windows

--- qrcode.py
+++ qrcode.py
@@ -9,13 +9,18 @@ import png
 from pyqrcode import QRCode
 from time import sleep
 
-os.system("clear")
+if os.name == 'posix':
+    cls = "clear"
+else:
+    cls = "cls"
+
+os.system(cls)
 
 #Função que gera QR Code de um link
 def qr_link():
 
     # Clear
-    os.system('clear')
+    os.system(cls)
 
     # Link desejado para o QRcode
     LINK = input('Cole aqui o link para gerar seu QRCode:\n\n')
@@ -29,19 +34,19 @@ def qr_link():
 def qr_wifi():
 
     #Clear
-    os.system("clear")
+    os.system(cls)
 
     # SSID desejado para o QRcode
     SSID = input('Cole aqui o SSID da rede para gerar seu QRCode:\n\n')
     #Clear
-    os.system("clear")
+    os.system(cls)
     # tipo de seguranca
     SEC = int(input("""Digite o tipo de segurança da rede wi-fi\n
 1) Nenhuma
 2) WEP
 3) WPA/WPA2-Personal\n\n"""))
     #Clear
-    os.system("clear")
+    os.system(cls)
     if SEC == 1:
         code = pyqrcode.create(f"WIFI:S:{SSID};T:nopass;P:;;")
         # Salva o QRCode gerado no local desejado
@@ -52,52 +57,52 @@ def qr_wifi():
         PASS = input("Cole a senha do wi-fi que deseja incluir no QRcode\n\n")
         code = pyqrcode.create(f"WIFI:S:{SSID};T:WEP;P:{PASS};H:False;;")
         #Clear
-        os.system("clear")
+        os.system(cls)
         PNG = input('\nDigite aqui o nome da sua imagem gerada (Exemplo: QRcode1.png):\n\n')
         code.png( f"{PNG}.png" , scale=8)
     elif SEC == 3:
         # senha da rede
         PASS = input("Cole a senha do wi-fi que deseja incluir no QRcode\n\n")
         #Clear
-        os.system("clear")
+        os.system(cls)
         code = pyqrcode.create(f"WIFI:S:{SSID};T:WPA;P:{PASS};H:False;;")
         PNG = input('Digite aqui o nome da sua imagem gerada (Exemplo: QRcode1.png):\n\n')
         code.png( f"{PNG}.png" , scale=8)
 
 
 def qr_email():
-    os.system("clear")
+    os.system(cls)
 
     DEST = input("Digite o destinatário do e-mail:\n\n")
 
-    os.system("clear")
+    os.system(cls)
 
     ASSUNTO = input("Digite o assunto do e-mail:\n\n")
 
-    os.system("clear")
+    os.system(cls)
 
     CONTEUDO = input("Digite aqui o conteudo do email:\n\n")
 
-    os.system("clear")
+    os.system(cls)
 
     code = pyqrcode.create(f"MATMSG:TO:{DEST};SUB:{ASSUNTO};BODY:{CONTEUDO};;")
     PNG = input('Digite aqui o nome da sua imagem gerada (Exemplo: QRcode1.png):\n\n')
     code.png( f"{PNG}.png" , scale=8)
 
 def basic_contact():
-    os.system("clear")
+    os.system(cls)
 
     P_NOME = str(input("Digite o primeiro nome do contato:\n\n"))
 
-    os.system("clear")
+    os.system(cls)
 
     S_NOME = str(input("Digite o sobrenome do contato:\n\n"))
     
-    os.system("clear")
+    os.system(cls)
 
     TEL = input("Digite o número de telefone do contato:\n\n")
 
-    os.system("clear")
+    os.system(cls)
 
     code = pyqrcode.create(f"""BEGIN:VCARD
 VERSION:2.1
@@ -110,31 +115,31 @@ END:VCARD""")
     code.png( f"{PNG}.png" , scale=8)
 
 def contact():
-    os.system("clear")
+    os.system(cls)
 
     P_NOME = str(input("Digite o primeiro nome do contato:\n\n"))
 
-    os.system("clear")
+    os.system(cls)
 
     S_NOME = str(input("Digite o sobrenome do contato:\n\n"))
     
-    os.system("clear")
+    os.system(cls)
 
     TEL = input("Digite o número de telefone do contato:\n\n")
 
-    os.system("clear")
+    os.system(cls)
 
     COMP = input("Digite o nome da empresa ou companhia do contato:\n\n")
 
-    os.system("clear")
+    os.system(cls)
 
     CARGO = input('Digite seu cargo dentro da empresa:\n\n')
 
-    os.system("clear")
+    os.system(cls)
 
     EMAIL = input("Digite o E-mail do contato:\n\n")
 
-    os.system("clear")
+    os.system(cls)
 
     code = pyqrcode.create(f"""BEGIN:VCARD
 VERSION:2.1
@@ -172,11 +177,11 @@ def init():
     else:
         print("Opção não encontrada, tente novamente:")
         sleep(2)
-        os.system("clear")
+        os.system(cls)
         init()
 
 try:
     init()
 except KeyboardInterrupt:
-    os.system('clear')
+    os.system(cls)
     exit()
